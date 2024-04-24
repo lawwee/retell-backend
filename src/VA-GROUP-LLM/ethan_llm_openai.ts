0acbfb7053ef0cd8f5dcfc9320e557f1d32fa8e1
Ganiyu Idris <golamide27@gmail.com>
2024-04-24 21:05:04 +0100

    Changes

--- src/VA-GROUP-LLM/ethan_llm_openai.ts
+++ src/VA-GROUP-LLM/ethan_llm_openai.ts
@@ -40,7 +40,7 @@ export class ethanDemoLlmClient {
 
     Step 2: "Hi ${firstname}," <break time="2s" /> "Ethan with Virtual Team Expert following up on an inquiry we recieved in the past for our virtual assistant services, are you still looking for help?", if user responds: "yes" (proceed to step 3), if user responds: "no", say: "No worries, please keep us in mind if anything changes." call function end_call to hang up. 
 
-    Step 3: "Great! I’d, love to set up a short zoom call with our sales manager to tailor our services and provide a custom quote.” <break time="2s" /> “Would next Wednesday at 8 AM work?", if user responds: "yes" (proceed to step 6), if user responds: "No", say: "would 10 AM work?", if user responds: "no" (proceed to step 4).
+    Step 3: "Great! I’d, love to set up a short zoom call with our sales manager to tailor our services and provide a custom quote.” “Would next Wednesday at 8 AM work?", if user responds: "yes" (proceed to step 6), if user responds: "No", say: "would 10 AM work?", if user responds: "no" (proceed to step 4).
 
     Step 4: Address common objections here with empathy and provide concise responses:
 
