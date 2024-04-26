c38513c05fb9deaf351f28dc86f52b8f94e3b2c0
Hydradaboss <hydradaboss06@gmail.com>
2024-04-26 15:19:18 +0100

    fix

--- src/server.ts
+++ src/server.ts
@@ -44,6 +44,7 @@ import { logsToCsv } from "./LOGS-FUCNTION/logsToCsv";
 import { statsToCsv } from "./LOGS-FUCNTION/statsToCsv";
 import { scheduleCronJob } from "./Schedule-Fuctions/scheduleJob";
 connectDb();
+console.log("connected to gcp")
 export class Server {
   public app: expressWs.Application;
   private httpServer: HTTPServer;
