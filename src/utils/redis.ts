5942097d93ac79281ccb5e79118a09c6d9567618
Hydradaboss <hydradaboss06@gmail.com>
2024-05-17 16:39:09 +0100

    changes to redis

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 export const redisClient = createClient({
-    password: process.env.REDIS_PASSOWORD,
+    password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
         port: 12658
